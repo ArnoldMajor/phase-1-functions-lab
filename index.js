883c5c186e1628455e58294acd12279d52f2aad3
ArnoldMajor <arnoldmwangakala@gmail.com>
2024-03-22 06:22:10 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+let streetNumber;
+let officeStreet = 42;
+
+function distanceFromHqInBlocks(streetNumber) {
+    return Math.abs(streetNumber - officeStreet);
+}
+
+let feetPerBlock = 264;
+
+function distanceFromHqInFeet(streetNumber){
+    return Math.abs(distanceFromHqInBlocks(streetNumber) * feetPerBlock);
+}
+
+let start;
+let destination;
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((destination - start) * feetPerBlock);
+}
+
+function calculatesFarePrice(start, destination){
+    if (distanceTravelledInFeet(start, destination) <= 400){
+        return 0;
+    }
+
+    else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000){
+        return Math.abs((2 * (distanceTravelledInFeet(start , destination)-400))/100);
+    }
+
+    else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500){
+        return 25;
+    }
+
+    else (distanceTravelledInFeet(start, destination) > 2500 ); {
+        return 'cannot travel that far'
+    }
+}
